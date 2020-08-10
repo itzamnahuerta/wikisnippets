9ee9b868209df7a61869bebe7de9ad5187ec8e88
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 19:13:05 -0400

    import styles sheet for component

--- src/components/GetArticles.jsx
+++ src/components/GetArticles.jsx
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/GetArticles.css';
 import axios from 'axios';
+
 
 export default class GetArticles extends Component {
   render() {
