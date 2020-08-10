dd3baf71803c289617f299d1a51045e0ba122c64
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 19:57:44 -0400

    added props searchquery to input value

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,6 +14,7 @@ function SearchBar(props) {
           type="text" 
           placeholder="Search" 
           aria-label="Search" 
+          value={props.searchQuery}
         />
       </MDBFormInline > 
     </MDBCol>
