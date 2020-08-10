ba9ca7a46bad067862076ea3295f7446050c1a15
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 19:56:52 -0400

    add searchbar elements

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,9 +3,20 @@ import { MDBCol, MDBFormInline, MDBIcon } from "mdbreact";
 
 function SearchBar(props) {
   return (
-    <div>
-      <p> search bar who? </p>
-    </div>
+    <MDBCol md="6" >
+      <MDBFormInline 
+        className="md-form" 
+      >
+        <MDBIcon icon="search" />
+        
+        <input 
+          className="form-control form-control-sm ml-3 w-75" 
+          type="text" 
+          placeholder="Search" 
+          aria-label="Search" 
+        />
+      </MDBFormInline > 
+    </MDBCol>
   )
 }
 
