4b7a2e26034c80d8daf848fd7c91ad2967372be9
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 23:53:11 -0400

    imported material bootstrap

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+
 
 function DisplayArticles(props) {
   // console.log("i am coming from display articles", props.data.data)
@@ -12,16 +14,32 @@ function DisplayArticles(props) {
       let results = props.data.data;
       // console.log("tryna render these articles though => ",results);
       return results.query.search.map(article => ( 
-      
-      <div key={article.timestamps}>
-        <h4>
-          <a 
+        <MDBCol>
+        <MDBCard style={{ width: "22rem", margin:'1em' }}>
+          <MDBCardBody>
+            <MDBCardTitle> 
+            <a 
             href={ `https://en.wikipedia.org/wiki/${article.title.replace(regex, "_")}`}>  
-          {article.title}
-          </a>
-        </h4>
-        <span dangerouslySetInnerHTML={{__html: article.snippet }}></span>
-      </div>
+            {article.title}
+            </a>
+            </MDBCardTitle>
+            <MDBCardText>
+            <span dangerouslySetInnerHTML={{__html: article.snippet }}></span>
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+
+
+      // <div key={article.timestamps}>
+      //   <h4>
+      //     <a 
+      //       href={ `https://en.wikipedia.org/wiki/${article.title.replace(regex, "_")}`}>  
+      //     {article.title}
+      //     </a>
+      //   </h4>
+      //   <span dangerouslySetInnerHTML={{__html: article.snippet }}></span>
+      // </div>
       )
       );
     } 
