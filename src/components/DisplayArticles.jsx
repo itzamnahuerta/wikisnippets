63fa3b3373c5338af8d06702de0b53f71a85186d
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 21:35:55 -0400

    set up render articles function with console log of target data

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 function DisplayArticles(props) {
+  console.log("i am coming from display articles", props.data.data)
+
+
+
+  let renderArticles = () => {
+    // console.log(props.loading)
+    if(props.loading) {
+      let results = props.data.data;
+      console.log("tryna render these articles though => ",results.query.search)
+      // props.data.data.map()
+    }
+  }
+
   return (
     <div>
       <p> I display Articles</p>
+      {renderArticles()}
     </div>
   )
 }
