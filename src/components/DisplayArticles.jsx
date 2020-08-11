f553493d45f1036422c68e0bf389c1ce6e3b6c2d
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-10 23:24:51 -0400

    added title and span snippet from data

--- src/components/DisplayArticles.jsx
+++ src/components/DisplayArticles.jsx
@@ -8,16 +8,20 @@ function DisplayArticles(props) {
   let renderArticles = () => {
     if(props.loading) {
       let results = props.data.data;
-      return results.query.search.map(a => <p> {a.title} </p>)
-
-      // console.log("tryna render these articles though => ",results.query.search)
-      // props.data.data.map()
+      console.log("tryna render these articles though => ",results);
+      return results.query.search.map(article => ( 
+      <div key={article.timestamps}>
+        <h4> {article.title} </h4>
+        <span dangerouslySetInnerHTML={{__html: article.snippet }}></span>
+      {/* <p> {article.snippet} </p> */}
+      </div>
+      )
+      );
     } 
   }
 
   return (
     <div>
-      <p> I display Articles</p>
       {renderArticles()}
     </div>
   )
